1f273c83320af6f5df0cfea821728909a13a0be9
Jadan Kerr <badi1863@gmail.com>
2023-10-04 17:55:43 -0600

    Changed list to Array and did some stuff

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,27 @@ public class Main {
     public static void main(String[] args) {
         boolean isTrue = true;
         Scanner scanner = new Scanner(System.in);
-        List<Integer> intArray = new ArrayList<>();
+        //Initializes new array with length of 100
+        int[] intArray = new int[100];
+        //initializes arrayLength counter variable
+        int arrayLength = 0;
 
         while (isTrue){
             System.out.println("What integer would you like to add to the array?");
             int newInt = scanner.nextInt();
+            //If input is not -1, add integer to array and increase arrayLength by one
             if (newInt !=-1) {
-                intArray.add(newInt);
+                intArray[arrayLength] = newInt;
+                arrayLength++;
             }
             else {
+                //if input is -1 end program
                 isTrue = false;
             }
         }
-        for (int i = intArray.size() - 1; i >-1; i-- ) {
-            System.out.println(intArray.get(i));
+        //Counts down from arrayLength, printing the values as it goes
+        for (int j = arrayLength - 1; j  >-1; j-- ) {
+            System.out.println(intArray[j]);
         }
     }
 }
